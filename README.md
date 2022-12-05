d7f21dd2a06397641a6a6668c9983c8c394f3ec2
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2022-12-05 09:44:46 +0900

    add library

--- README.md
+++ README.md
@@ -4,6 +4,32 @@
 
 ![](./infra.drawio.svg)
 
+## 環境構築
+
+1. クローンする
+
+```
+git clone git@github.com:hiromu-ikeda/gesalon-web.git
+```
+
+2. Docker 環境をビルドする
+
+```
+sh local_docker.sh build
+```
+
+3. コンテナを立ち上げる
+
+```
+sh local_docker.sh up
+```
+
+※コンテナを止めるとき
+
+```
+sh local_docker.sh down
+```
+
 ## 概要
 
 YNot と称するオンラインコミュティです。
--- local_docker.sh
+++ local_docker.sh
@@ -2,10 +2,18 @@
 
 cd ./infra/docker
 
-docker-compose build
-
-if [ ! -e ../node_modules ]; then
-  docker-compose run --rm web sh -c "npm install"
+if [ $1 == "build" ]; then
+  docker-compose build
 fi
 
-docker-compose up -d
+if [ $1 == "up" ]; then
+  if [ ! -e ../node_modules ]; then
+    docker-compose run --rm web sh -c "npm install"
+  fi
+
+  docker-compose up -d
+fi
+
+if [ $1 == "down" ]; then
+  docker-compose stop
+fi
