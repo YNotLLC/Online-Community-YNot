30af54a421b44c6763e33a81ecfd7349374ed4ee
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2022-12-06 16:37:36 +0900

    add Header

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,16 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Flex bg="teal.400" justify="space-between" alignItems="center" p={4}>
+      <Heading as="h1" color="gray.50" size="xl">
+        YNot
+      </Heading>
+      <Text color="gray.50" fontWeight="bold">
+        テスト太郎さん
+      </Text>
+    </Flex>
+  );
+};
+
+export default Header;
