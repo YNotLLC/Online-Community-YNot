f28c0fb6a7a3a6ab7cab715a515268a39c9f3171
DESKTOP-1LQQMTC\inoue <ryu.propagate@gmail.com>
2022-12-06 17:16:50 +0900

    typo修正

--- frontend/lib/apollo/apollo.ts
+++ frontend/lib/apollo/apollo.ts
@@ -3,6 +3,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 export { ApolloProvider };
 
 export const client = new ApolloClient({
-  uri: proccess.env.NEXT_PUBLIC_WORDPRESS_ENDPOINT,
+  uri: process.env.NEXT_PUBLIC_WORDPRESS_ENDPOINT,
   cache: new InMemoryCache(),
 });
